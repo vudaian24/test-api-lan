488b3718142fd376a3822cb26ddb9ab2e43ee210
vudaian24 <vudaian12a7@gmail.com>
2025-05-28 13:59:21 +0700

    ..

--- api/index.ts
+++ api/index.ts
@@ -1,16 +1,27 @@
+import express from 'express';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '../src/app.module';
 import { ExpressAdapter } from '@nestjs/platform-express';
-import express from 'express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { INestApplication } from '@nestjs/common';
 
 const server = express();
-
 let cachedApp: INestApplication | null = null;
 
 async function bootstrap() {
   if (!cachedApp) {
     const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+
+    // Cấu hình Swagger
+    const config = new DocumentBuilder()
+      .setTitle('API Documentation')
+      .setDescription('API description')
+      .setVersion('1.0')
+      .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('swagger', app, document);
+
     await app.init();
     cachedApp = app;
   }
