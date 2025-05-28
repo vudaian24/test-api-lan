828663f7455c6addde30a04941e8f512f5ccc56c
vudaian24 <vudaian12a7@gmail.com>
2025-05-28 13:35:38 +0700

    ..

--- api/index.ts
+++ api/index.ts
@@ -1,19 +1,77 @@
-// api/index.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '../src/app.module';
-import serverlessExpress from '@vendia/serverless-express'; // Đảm bảo bạn đang dùng @vendia/serverless-express, không phải @codegenie/serverless-express
+import { VercelRequest, VercelResponse } from '@vercel/node';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express from 'express';
+import helmet from 'helmet';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
-let cachedServer;
+const ConfigKey = {
+  PORT: 'PORT',
+  BASE_PATH: 'BASE_PATH',
+  SWAGGER_ENABLED: 'SWAGGER_ENABLED',
+};
+const BooleanString = {
+  TRUE: 'true',
+  FALSE: 'false',
+};
 
-// Hàm handler được export mặc định cho Vercel
-export default async function (req, res) {
-  if (!cachedServer) {
-    const nestApp = await NestFactory.create(AppModule);
+const expressApp = express();
+let nestApp;
+
+async function bootstrap() {
+  if (!nestApp) {
+    nestApp = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
+
+    // Middleware security
+    nestApp.use(helmet());
+
+    const configService = nestApp.get(ConfigService);
+
+    // CORS config
+    const whiteList = '*';
+    const corsOptions: CorsOptions = {
+      origin:
+        whiteList?.split(',')?.length > 1
+          ? whiteList.split(',')
+          : whiteList,
+      allowedHeaders: [
+        'Content-Type',
+        'Authorization',
+        'Language',
+        'X-Timezone',
+        'X-Timezone-Name',
+        'X-Mssp-Id',
+        'X-Organization-Id',
+      ],
+      optionsSuccessStatus: 200,
+      methods: ['GET', 'PUT', 'POST', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+    };
+    nestApp.enableCors(corsOptions);
+
+    // Global prefix
+    nestApp.setGlobalPrefix(configService.get(ConfigKey.BASE_PATH) ?? '');
+
+    // Swagger setup (nếu bật)
+    if (configService.get(ConfigKey.SWAGGER_ENABLED) === BooleanString.TRUE) {
+      const config = new DocumentBuilder()
+        .addBearerAuth()
+        .setTitle('Cybereason MDR Mobile App 2.0 project - Platform API')
+        .setDescription('Provides RESTful API to internal & external services')
+        .setVersion('v1')
+        .build();
+
+      const document = SwaggerModule.createDocument(nestApp, config);
+      SwaggerModule.setup('swagger', nestApp.getHttpAdapter().getInstance(), document);
+    }
+
     await nestApp.init();
-    cachedServer = serverlessExpress({ app: nestApp.getHttpAdapter().getInstance() });
   }
+}
 
-  // Gọi hàm handler đã được tạo, truyền vào req, res của Vercel
-  // serverlessExpress sẽ tự động chuyển đổi req, res của Vercel thành event, context, callback
-  return cachedServer(req, res);
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+  await bootstrap();
+  expressApp(req, res);
 }
