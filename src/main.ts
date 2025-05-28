ed82d8975c69763ca7022c5da30331184150ae0f
vudaian24 <vudaian12a7@gmail.com>
2025-05-28 12:08:10 +0700

    ..

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,40 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   const config = new DocumentBuilder()
     .setTitle('User CRUD API')
     .setDescription('API for managing users')
     .setVersion('1.0')
-    .build(); 
+    .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/swagger', app, document);
+
+  app.use(helmet());
+
+  const corsOptions: CorsOptions = {
+    origin: '*',
+    allowedHeaders: [
+      'Content-Type',
+      'Authorization',
+      'Language',
+      'X-Timezone',
+      'X-Timezone-Name',
+      'X-Mssp-Id',
+      'X-Organization-Id',
+    ],
+    optionsSuccessStatus: 200,
+    methods: ['GET', 'PUT', 'POST', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+  };
+
+  app.enableCors(corsOptions);
+
   await app.listen(5000);
+  console.log(`Application is running on: http://localhost:${5000}`);
 }
 bootstrap();
