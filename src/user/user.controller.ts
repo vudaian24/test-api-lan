563203dafbeb9dacdef072adfed682f18719fe55
maithilan2512 <nhatlan25122002@gmail.com>
2025-05-29 10:24:53 +0700

    f

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -25,6 +25,11 @@ export class UserController {
     return this.userService.findOne(+id);
   }
 
+  @Patch('edit/:id')
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.userService.update(+id, updateUserDto);
+  }
+
   @Delete('delete/:id')
   remove(@Param('id') id: string) {
     return this.userService.remove(+id);
